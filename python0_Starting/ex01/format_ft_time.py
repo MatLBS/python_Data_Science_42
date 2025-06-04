cec543566bdd33f64a3359f51e8a540aac744b39
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-06-04 16:03:37 +0200

    I did the first 3 exercices

---
+++ python0_Starting/ex01/format_ft_time.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import time
+
+now = datetime.now()
+
+seconds = time.time()
+scientific_notation="{:.2e}".format(seconds)
+
+print("Seconds since January 1, 1970: ", seconds, "or", scientific_notation, "in scientific notation")
+print(now.strftime("%b %d %Y"))
+
---
+++ python0_Starting/ex02/find_ft_type.py
@@ -0,0 +1,21 @@
+def all_thing_is_obj(object: any) -> int:
+
+  obj = type(object)
+
+  if (obj == list):
+    print("List : ", obj)
+  elif (obj == tuple):
+    print("Tuple : ", obj)
+  elif(obj == set):
+    print("Set : ", obj)
+  elif(obj == dict):
+    print("Dict : ", obj)
+  elif(obj == str):
+    print(object, "is in the kitchen : ", obj)
+  else:
+    print("Type not found")
+  return 42
+
+
+
+  
