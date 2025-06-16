60faf57a957377fb0b15b5ea10e7ac71461ddcb1
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-06-16 19:41:56 +0200

    finished python_4

--- python_4_Dod/ex01/in_out.py
+++ python_4_Dod/ex01/in_out.py
@@ -1,45 +1,70 @@
 def square(x: int | float) -> int | float:
-	assert isinstance(x, (int, float)), "The value must be integer or float"
-	assert x != 0, "You can not divide by 0"
-	return x**2
+    """
+    Function that returns the square of a number.
+    Args:
+        x (int | float): The number to be squared.
+    Returns:
+        int | float: The square of the input number.
+    """
+    assert isinstance(x, (int, float)), "The value must be integer or float"
+    assert x != 0, "You can not divide by 0"
+    return x**2
+
 
 def pow(x: int | float) -> int | float:
-	return x**x
+    """
+    Function that returns the power of a number raised to itself.
+    Args:
+        x (int | float): The number to be raised to the power of itself.
+    Returns:
+        int | float: The result of raising the input
+        number to the power of itself.
+    """
+    return x**x
 
-# def outer(x: int | float, function) -> object:
-# 	def inner() -> float:
-# 		count = 0
-# 		if count == 0:
-# 			count = function(x)
-# 		else:
-# 			count = function(count)
-# 		return count
-# 	return inner
 
 def outer(x: int | float, function) -> object:
-	current = x
+    """
+    Create a counter function that performs calculations and
+    updates its input value.
+    Args:
+        x (int | float): The initial value to be processed.
+        function: A function that takes a single argument and returns a value.
+    Returns:
+        object: A callable function that, when invoked,
+        applies the given function
+        to the current value of x and updates x with the result.
+    """
+    def inner() -> float:
+        """
+        Inner function that applies the provided function
+        to the current value of x
+        and updates x with the result.
+        Returns:
+            float: The result of applying the function to x.
+        """
+        nonlocal x
+        result = function(x)
+        x = result
+        return result
 
-	def inner() -> float:
-		nonlocal current
-		current = function(current)
-		return current
+    return inner
 
-	return inner
 
 def main():
-	try:
-		my_counter = outer(3, square)
-		print(my_counter())
-		print(my_counter())
-		print(my_counter())
-		print("---")
-		another_counter = outer(1.5, pow)
-		print(another_counter())
-		print(another_counter())
-		print(another_counter())
-	except AssertionError as error:
-		print("AssertionError:", error)
+    try:
+        my_counter = outer(3, square)
+        print(my_counter())
+        print(my_counter())
+        print(my_counter())
+        print("---")
+        another_counter = outer(1.5, pow)
+        print(another_counter())
+        print(another_counter())
+        print(another_counter())
+    except AssertionError as error:
+        print("AssertionError:", error)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- python_4_Dod/ex02/callLimit.py
+++ python_4_Dod/ex02/callLimit.py
@@ -1,29 +1,59 @@
 def callLimit(limit: int):
-	count = 0
-	def callLimiter(function):
-		def limit_function(*args: any, **kwds: any):
-			print(args)
+    """
+    Decorator to limit the number of times a function can be called.
+    Args:
+        limit (int): The maximum number of times the function can be called.
+    Returns:
+        A decorator that limits the number of calls to the decorated function.
+    """
+    count = 0
 
+    def callLimiter(function):
+        """
+        Inner function that wraps the original function to
+        enforce the call limit.
+        Args:
+            function: The function to be decorated.
+        Returns:
+            A wrapper function that checks the call count and
+            raises an error if the limit is exceeded."""
+        def limit_function(*args: any, **kwds: any):
+            """
+            Wrapper function that checks the call count and
+            raises an error if the limit is exceeded.
+            Args:
+                *args: Variable length argument list for the function.
+                **kwds: Keyword arguments for the function.
+            Returns:
+                The result of the original function if the
+                call limit is not exceeded.
+            """
+            try:
+                nonlocal count
+                count += 1
+                if count <= limit:
+                    return function(*args, **kwds)
+                else:
+                    raise AssertionError(f"{function} call too many times")
+            except AssertionError as error:
+                print("Error:", error)
+        return limit_function
+    return callLimiter
 
 
 def main():
-	try:
-		@callLimit(3)
-		def f():
-			print("f()")
+    @callLimit(3)
+    def f():
+        print("f()")
 
+    @callLimit(1)
+    def g():
+        print("g()")
 
-		@callLimit(1)
-		def g():
-			print("g()")
-
-
-		for i in range(3):
-			f()
-			g()
-	except AssertionError as error:
-		print("AssertionError:", error)
+    for i in range(3):
+        f()
+        g()
 
 
 if __name__ == "__main__":
-	main()
+    main()
